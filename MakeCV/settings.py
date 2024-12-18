4e595d06cf596e32f3fa7b4af60cf9198626eb3e
Gilbert <www.ngouogilbert@gmail.com>
2024-12-18 18:12:00 +0000

    changement de BD

--- MakeCV/settings.py
+++ MakeCV/settings.py
@@ -78,15 +78,8 @@ WSGI_APPLICATION = 'MakeCV.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root',
-        'PASSWORD': '',
-        'NAME': 'cv',
-        'PORT': '3306',
-        'HOST' : "127.0.0.1",
-        },
-    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
