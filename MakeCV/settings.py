6aeef71cd39650ef35d3f3417ceffdf8080b9ff0
Gilbert <www.ngouogilbert@gmail.com>
2024-12-22 20:01:17 +0000

    test

--- MakeCV/settings.py
+++ MakeCV/settings.py
@@ -25,9 +25,9 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-coq6b&p6q6zs)u=!&k(^q&1w48s=@=&m_ba=ut+w@i1e^c#v=q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['makecv.onrender.com']
 
 
 # Application definition
