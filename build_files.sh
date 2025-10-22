9abb68fda8e71ef04ea2600735326b65b50607b9
Gilbert Ngouo <149798152+Ngouo@users.noreply.github.com>
2025-10-22 23:43:15 +0100

    suppression de sh

--- build_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pip install -r requirements.txt
-
-# Configuration PyMySQL
-echo "import pymysql\npymysql.install_as_MySQLdb()" >> MakeCV/__init__.py
-
-# Collecte des fichiers statiques
-python manage.py collectstatic --noinput
