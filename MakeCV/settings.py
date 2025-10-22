919ebca917514fb82eecac1d06fbac9bfdb3101e
Gilbert Ngouo <149798152+Ngouo@users.noreply.github.com>
2025-10-22 22:09:03 +0100

    ajout de build

--- MakeCV/settings.py
+++ MakeCV/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'whitenoise.runserver_nostatic'
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
