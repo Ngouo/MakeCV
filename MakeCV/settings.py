1fd4a58687603b80bc3b8c7fd61ecd49358cecaa
Gilbert Ngouo <149798152+Ngouo@users.noreply.github.com>
2025-12-05 12:26:05 +0100

    host

--- MakeCV/settings.py
+++ MakeCV/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': 'makecv',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
 }
 }
